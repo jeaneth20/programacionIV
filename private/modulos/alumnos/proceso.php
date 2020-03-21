761a4ccba61cc355f516317bff5fbcc4a481e702
jeaneth20 <avaloskarina265@gmail.com>
2020-03-21 13:00:41 -0600

    app-academica

--- private/modulos/alumnos/proceso.php
+++ private/modulos/alumnos/proceso.php
@@ -1,42 +1,42 @@
 <?php 
 include('../../config/config.php');
-$docente = new docente($conexion);
+$alumno = new alumno($conexion);
 
 $proceso = '';
 if( isset($_GET['proceso']) && strlen($_GET['proceso'])>0 ){
 	$proceso = $_GET['proceso'];
 }
-$docente->$proceso( $_GET['docente'] );
-print_r(json_encode($docente->respuesta));
+$alumno->$proceso( $_GET['alumno'] );
+print_r(json_encode($alumno->respuesta));
 
-class docente{
+class alumno{
     private $datos = array(), $db;
     public $respuesta = ['msg'=>'correcto'];
     
     public function __construct($db){
         $this->db=$db;
     }
-    public function recibirDatos($docente){
-        $this->datos = json_decode($docente, true);
+    public function recibirDatos($alumno){
+        $this->datos = json_decode($alumno, true);
         $this->validar_datos();
     }
     private function validar_datos(){
         if( empty($this->datos['codigo']) ){
-            $this->respuesta['msg'] = 'por favor ingrese el codigo del docente';
+            $this->respuesta['msg'] = 'por favor ingrese el codigo del estudiante';
         }
         if( empty($this->datos['nombre']) ){
-            $this->respuesta['msg'] = 'por favor ingrese el nombre del docente';
+            $this->respuesta['msg'] = 'por favor ingrese el nombre del estudiante';
         }
         if( empty($this->datos['direccion']) ){
-            $this->respuesta['msg'] = 'por favor ingrese la direccion del docente';
+            $this->respuesta['msg'] = 'por favor ingrese la direccion del estudiante';
         }
-        $this->almacenar_docente();
+        $this->almacenar_alumno();
     }
-    private function almacenar_docente(){
+    private function almacenar_alumno(){
         if( $this->respuesta['msg']==='correcto' ){
             if( $this->datos['accion']==='nuevo' ){
                 $this->db->consultas('
-                    INSERT INTO docentes (codigo,nombre,direccion,telefono) VALUES(
+                    INSERT INTO alumnos (codigo,nombre,direccion,telefono) VALUES(
                         "'. $this->datos['codigo'] .'",
                         "'. $this->datos['nombre'] .'",
                         "'. $this->datos['direccion'] .'",
