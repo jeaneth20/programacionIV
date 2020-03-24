7b9c9df34d34996d5cb55a04e745caa451d552eb
jeaneth20 <avaloskarina265@gmail.com>
2020-03-24 17:14:34 -0600

    app-js

--- public/js/app.js
+++ public/js/app.js
@@ -1,27 +1,39 @@
-function init(){
-    var $ = el => {
-        return el.match(/^#/) ? document.querySelector(el) : document.querySelectorAll(el);
-    };
-    var mostrarVista = $("[class*='mostrar']");
-    mostrarVista.forEach(element => {
-        element.addEventListener('click',e=>{
-            e.stopPropagation();
+var $ = el => document.querySelector(el);
+document.addEventListener("DOMContentLoaded", event => {
+   
+    let alumnos = document.getElementById("alumnos"); // Instanciamos los elementos
+    let docentes = document.getElementById("docentes"); //
 
-            let modulo = e.srcElement.dataset.modulo,
-                form   = e.srcElement.dataset.form;
-            fetch(`public/vistas/${modulo}/${form}.html`).then( resp=>resp.text() ).then(resp=>{
-                $(`#vistas-${form}`).innerHTML = resp;
+    //SE AGREGAN LOS EVENTOS LISTENER PARA CADA VISTA
+    alumnos.addEventListener("click", e=>{
+        e.stopPropagation();
+        let modulo = "alumnos";
+        colocarVista(modulo);
 
-                let btnCerrar = $(`#btn-close-${form}`);
-                btnCerrar.addEventListener("click",event=>{
-                    $(`#vistas-${form}`).innerHTML = "";
-                });
-                import(`../vistas/${modulo}/${form}.js`).then(module=>{
-                    module.modulo();
-                });
-                init();
-            });
-        });
     });
-}
-init();
+
+    docentes.addEventListener("click", e=>{
+        e.stopPropagation();
+        let modulo = "docentes";
+        colocarVista(modulo);
+    
+    });
+}); 
+ function colocarVista(modulo){
+
+    fetch(`public/vistas/${modulo}/${modulo}.html`).then( resp => resp.text()).then( resp => {
+
+        document.getElementById(`vistas-${modulo}`).innerHTML = resp;
+        let btnCerrar = $(".close");
+
+        btnCerrar.addEventListener("click", event => {
+            $(`#vistas-${modulo}`).innerHTML = "";
+        });
+
+        let cuerpo = $("body"), script = document.createElement("script");
+        script.src = `public/vistas/${modulo}/${modulo}.js`;
+        cuerpo.appendChild(script);
+            
+    });
+
+ }
