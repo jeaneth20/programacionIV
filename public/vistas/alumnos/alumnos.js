154a3b9352422568e6d75cd7516491420eb24925
jeaneth20 <avaloskarina265@gmail.com>
2020-03-29 23:18:58 -0600

    kary

--- public/vistas/alumnos/alumnos.js
+++ public/vistas/alumnos/alumnos.js
@@ -1,30 +1,35 @@
 
     var $ = el => document.querySelector(el),
-    frmAlumnos = $("#frmAlumnos");
-frmAlumnos.addEventListener("submit",e=>{
-    e.preventDefault();
-    e.stopPropagation();
+        frmAlumnos = $("#frmAlumnos");
+    frmAlumnos.addEventListener("submit",e=>{
+        e.preventDefault();
+        e.stopPropagation();
+        
+        let alumnos = {
+            accion    : frmAlumnos.dataset.accion,
+            idAlumno : frmAlumnos.dataset.idAlumno,
+            accion : 'nuevo',
+            codigo    : $("#txtCodigoAlumno").value,
+            nombre    : $("#txtNombreAlumno").value,
+            direccion : $("#txtDireccionAlumno").value,
+            telefono  : $("#txtTelefonoAlumno").value
+        };
+        console.log(alumnos);
+        fetch(`private/modulos/alumnos/procesos.php?proceso=recibirDatos&alumno=${JSON.stringify(alumnos)}`).then( resp=>resp.json()).then(resp=>{
+            
+            console.log(resp);
+            $("#respuestaAlumno").innerHTML = `
+                <div class="alert alert-success" role="alert">
+                    ${resp.msg}
+                </div>
+            `;
+        });
+    });
+    frmAlumnos.addEventListener("reset",e=>{
+        $("#frm-alumnos").dataset.accion = 'nuevo';
+        $("#frm-alumnos").dataset.idAlumno = '';
     
-    let alumnos = {
-        accion    : frmAlumnos.dataset.accion,
-        idAlumno  : frmAlumnos.dataset.idalumno,
-        accion    : 'nuevo',
-        codigo    : $("#txtCodigoAlumno").value,
-        nombre    : $("#txtNombreAlumno").value,
-        direccion : $("#txtDireccionAlumno").value,
-        telefono  : $("#txtTelefonoAlumno").value
-    };
-    fetch(`private/modulos/alumnos/procesos.php?proceso=recibirDatos&alumno=${JSON.stringify(alumnos)}`).then( resp=>resp.json() ).then(resp=>{
-        $("#respuestaAlumno").innerHTML = `
-            <div class="alert alert-success" role="alert">
-                ${resp.msg}
-            </div>
-        `;
-    });
-});
-frmAlumnos.addEventListener("reset",e=>{
-    $("#frm-alumnos").dataset.accion = 'nuevo';
-    $("#frm-alumnos").dataset.idalumno = '';
-
-    });
+        });
+    
+    
   
